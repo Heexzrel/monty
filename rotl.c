5fc1387eb5668a5740c1af0213a5d55f02c7d104
George Israel <georgeisrael18@gmail.com>
2023-08-24 21:39:20 -0700

    monty

---
+++ rotl.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+  *f_rotl- rotates the stack form bottom  to the top
+  *@head: stack head
+  *
+  *Return: no return
+ */
+void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
+{
+	stack_t *tmp = *head, *aux;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	aux = (*head)->next;
+	aux->prev = NULL;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = *head;
+	(*head)->next = NULL;
+	(*head)->prev = tmp;
+	(*head) = aux;
+}
